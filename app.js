cd006f110126bd6c702134780447e7641f757c55
8eatles <8eatles@siliconcube.co.kr>
2018-11-02 17:06:51 +0900

    Enable RESTful API

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 const Sequelize = require("sequelize");
+const restify = require('restify');
 
-const dbconf = JSON.parse(fs.readFileSync('config.json', 'utf8')).database;
+// Reading Config
+const conf = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const serverconf = conf.server;
+const dbconf = conf.database;
+
+// Initing DB Connection
 const sequelize = new Sequelize(dbconf.database, dbconf.user, dbconf.password, {
   host: dbconf.host,
   dialect: "mysql",
@@ -15,6 +21,13 @@ const sequelize = new Sequelize(dbconf.database, dbconf.user, dbconf.password, {
   }
 });
 
+// Checking DB Connection
+sequelize.authenticate().catch(err => {
+	console.error('Unable to connect to the database:', err);
+	process.exit();
+});
+
+// Defining Database Models
 const User = sequelize.define("user", {
   id: {
     type: Sequelize.INTEGER,
@@ -34,16 +47,97 @@ const User = sequelize.define("user", {
   }
 });
 
-sequelize.authenticate().catch(err => {
-	console.error('Unable to connect to the database:', err);
-	process.exit();
+// Building Database Structure
+User.sync({force: true}).then(() => {});
+
+// Create API Server
+let server = restify.createServer();
+
+// Enable Body Data Parser for POST Request
+server.use(restify.plugins.bodyParser());
+
+// Select All Users
+server.get('/user', function(req, res, next) {
+	User.findAll().then(users => {
+		res.send({
+			status: 0,
+			message: '',
+			data: users
+		});
+	}).catch(() => {
+		res.send({
+			status: 1,
+			message: 'there is no users',
+			data: {}
+		});
+	});  
+  return next();
 });
 
-User.sync({force: true}).then(() => {
-	User.create({ firstname: "Kim", lastname: "Jongchan" });
-	User.create({ firstname: "Lee", lastname: "Minhwan" }).then(() => {
-		User.findAll().then(users => {
-			console.log(users);
-		})
-	});
+// Select Special User
+server.get('/user/:id', function(req, res, next) {
+	User.findById(req.params.id).then(user => {
+		res.send({
+			status: 0,
+			message: '',
+			data: user
+		});
+	}).catch(() => {
+		res.send({
+			status: 1,
+			message: 'cannot find this id.',
+			data: {}
+		});
+	});  
+  return next();
 });
+
+// Create USer
+server.post('/user', function(req, res, next) {
+	//parameter check;
+	if(req.params.firstname == null || req.params.firstname == '')
+	{
+		res.send({
+			status: 2,
+			message: 'firstname is empty.',
+			data: {}
+		});
+	}
+	else if(req.params.lastname == null || req.params.lastname == '')
+	{
+		res.send({
+			status: 3,
+			message: 'lastname is empty.',
+			data: {}
+		});
+	}
+	else
+	{
+		User.create({ firstname: req.params.firstname, lastname: req.params.lastname, gender: (req.params.gender == 'female') ? 'female' : 'male' }).then(user =>{
+			res.send({
+				status: 0,
+				message: 'created.',
+				data: user
+			});
+		}).catch(()=>{
+			res.send({
+				status: 1,
+				message: 'db error',
+				data: {}
+			});
+		});
+	}
+  return next();
+});
+
+// API Server Start
+server.listen(serverconf.port, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
+
+
+
+
+
+
